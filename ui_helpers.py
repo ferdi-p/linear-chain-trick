18ee6ca784cfe61b9b4b7a3e80a02e082dd37459
ferdi-p <57968931+ferdi-p@users.noreply.github.com>
2025-10-06 14:01:11 -0700

    linear chain trick demo

--- ui_helpers.py
+++ ui_helpers.py
@@ -51,5 +51,5 @@ def make_echart_card(title: str, height: str = '70vh'):
             'series': [
                 {'type': 'line', 'showSymbol': False, 'areaStyle': {}, 'data': []},
             ],
-        }).style('height:65vh;width:100%')
+        }).style('height:50vh;width:100%')
         return chart
